40f800a59b6665fdaf96de0c584471696a24ce17
echowht <echowht@qq.com>
2019-03-10 23:35:14 +0800

    通过http获取网络上的图片

--- lib/dog_model.dart
+++ lib/dog_model.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 class Dog{
   final String name;
   final String location;
@@ -7,5 +11,24 @@ class Dog{
 
   int rating =10;
 
-  Dog(this.name,this.location,this.description)
+  Dog(this.name,this.location,this.description);
+
+  Future getImageUrl() async{
+
+    if(imageUrl!=null){
+      return;
+    }
+    HttpClient http = new HttpClient();
+    try{
+      var uri=Uri.http('dog.ceo','/api/breeds/image/random');
+      var request= await http.getUrl(uri);
+      var response=await request.close();
+      var responseBody=await response.transform(utf8.decoder).join();
+      imageUrl = json.decode(responseBody)['message'];
+    }catch(e){
+      print(e);
+    }
+
+  }
 }
+
