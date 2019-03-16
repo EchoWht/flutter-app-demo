8b77de4850ecd6b1236500e3f1c533ec2647566c
echowht <echowht@qq.com>
2019-03-16 10:57:57 +0800

    显示对象的其他属性，并美化ui

--- lib/dog_card.dart
+++ lib/dog_card.dart
@@ -22,23 +22,66 @@ class _DogCardState extends State<DogCard>{
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Container(
-      child: dogImage,
+      height: 115.0,
+      child: Stack(
+        children: <Widget>[
+          Positioned(child: dogCard,left: 50.0,),
+          Positioned(child: dogImage,top: 7.5,)
+        ],
+      ),//栈，我暂时理解为列表的一种，必需包含children
     );
   }
 
   String renderUrl;
   ///dog 图片的显示Widget
-  Widget get dogImage{
+  Widget get dogImage {
     return Container(
       width: 100.0,
       height: 100.0,
       decoration: BoxDecoration(
-        shape:BoxShape.circle,//形状-圆形
-        image: DecorationImage(
-            image: NetworkImage(renderUrl ?? ''),
-            fit: BoxFit.cover
-        )
-      ),//装饰
+          shape: BoxShape.circle, //形状-圆形
+          image: DecorationImage(
+              image: NetworkImage(renderUrl ?? ''),
+              fit: BoxFit.cover
+          )
+      ), //装饰
+    );
+  }
+
+  ///显示dog其他信息的Widget
+  Widget get dogCard{
+    return Container(
+      width: 290.0,
+      height: 115.0,
+      child: Card(
+        color: Colors.orange,
+        child: Padding(padding: const EdgeInsets.only(
+            top: 8.0,
+            bottom: 8.0,
+            left: 64.0
+        ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,//交叉轴对齐：交叉轴对齐开始，
+            mainAxisAlignment: MainAxisAlignment.spaceAround,//主轴对齐：主轴对齐,空出周围
+            children: <Widget>[
+              Text(widget.dog.name,
+                style: Theme.of(context).textTheme.headline
+              ),
+              Text(widget.dog.location,
+                style: Theme.of(context).textTheme.subhead
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(
+                      Icons.star
+                  ),
+                  Text(':${widget.dog.rating / 10}')
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 
