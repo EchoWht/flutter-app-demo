d1d39df1debb8e424c1da4f9124ba2db0931b555
echowht <echowht@qq.com>
2019-03-16 10:15:03 +0800

    异步的从服务器获取图片资源

--- lib/dog_card.dart
+++ lib/dog_card.dart
@@ -16,10 +16,47 @@ class DogCard extends StatefulWidget{
 class _DogCardState extends State<DogCard>{
   Dog dog;
   _DogCardState(this.dog);
+
+  //将dog的一些属性显示在界面上
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Text(this.dog.name);
+    return new Container(
+      child: dogImage,
+    );
+  }
+
+  String renderUrl;
+  ///dog 图片的显示Widget
+  Widget get dogImage{
+    return Container(
+      width: 100.0,
+      height: 100.0,
+      decoration: BoxDecoration(
+        shape:BoxShape.circle,//形状-圆形
+        image: DecorationImage(
+            image: NetworkImage(renderUrl ?? ''),
+            fit: BoxFit.cover
+        )
+      ),//装饰
+    );
+  }
+
+  /// initState 会在state创建时执行
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    renderDogPic();//调用异步获取图片的方法
+
+  }
+
+  ///异步的去服务器获取图片
+  void renderDogPic() async{
+    await dog.getImageUrl();
+    setState(() {
+      renderUrl=dog.imageUrl;
+    });
   }
 
 }
